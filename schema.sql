187437178b3acc9daafae58c18362947338e2337
touch1295 <69025171+touch1295@users.noreply.github.com>
2020-10-23 20:02:25 -0400

    adjust commit

--- schema.sql
+++ schema.sql
@@ -27,49 +27,49 @@ DROP TABLE IF EXISTS us_covid;
 -- Create tables 
 CREATE TABLE us_covid
 (
-    HCP_ID Serial Primary Key,
-    county TEXT NOT NULL,
+    index Serial
+    Primary Key,
+    state TEXT NOT NULL,
     county_fips INT NOT NULL,
-    confirmed_cases INT NOT NULL,
-    confirmed_deaths INT NOT NULL,
-    _type VARCHAR NOT NULL
+    fips INT NOT NULL,
+    cases INT NOT NULL,
+    deaths INT NOT NULL,
+    Type VARCHAR NOT NULL
 );
 
-select *
-from us_covid
+    select *
+    from us_covid
 
 
 
 
---drop table if exists 
-DROP TABLE IF EXISTS national_history;
+    --drop table if exists 
+    DROP TABLE IF EXISTS national_history;
 
--- Create tables 
-CREATE TABLE national_history
-(
-    HCP_ID Serial Primary Key,
-    date VARCHAR NOT NULL,
-    death INT NOT NULL,
-    deathIncrease INT NOT NULL,
-    inIcuCumulative INT NOT NULL,
-    inIcuCurrently INT NOT NULL,
-    hospitalizedIncrease INT NOT NULL,
-    hospitalizedCurrently INT NOT NULL,
-    hospitalizedCumulative INT NOT NULL,
-    negative INT NOT NULL,
-    negativeIncrease INT NOT NULL,
-    onVentilatorCumulative INT NOT NULL,
-    onVentilatorCurrently INT NOT NULL,
-    posNeg INT NOT NULL,
-    positive INT NOT NULL,
-    positiveIncrease INT NOT NULL,
-    recovered INT NOT NULL,
-    states INT NOT NULL,
-    totalTestResults INT NOT NULL,
-    totalTestResultsIncrease INT NOT NULL,
-    _type VARCHAR NOT NULL
-);
+    -- Create tables 
+    CREATE TABLE national_history
+    (
+        date VARCHAR NOT NULL,
+        death INT NOT NULL,
+        deathIncrease INT NOT NULL,
+        inIcuCumulative INT NOT NULL,
+        inIcuCurrently INT NOT NULL,
+        hospitalizedIncrease INT NOT NULL,
+        hospitalizedCurrently INT NOT NULL,
+        hospitalizedCumulative INT NOT NULL,
+        negative INT NOT NULL,
+        negativeIncrease INT NOT NULL,
+        onVentilatorCumulative INT NOT NULL,
+        onVentilatorCurrently INT NOT NULL,
+        posNeg INT NOT NULL,
+        positive INT NOT NULL,
+        positiveIncrease INT NOT NULL,
+        recovered INT NOT NULL,
+        states INT NOT NULL,
+        totalTestResults INT NOT NULL,
+        totalTestResultsIncrease INT NOT NULL,
+        _Type VARCHAR NOT NULL
+    );
 
-select *
-from national_history
-
+    select *
+    from national_history
